38e34eeccbc1ab0d721d2e8846f51a195d304def
Diana Lizeth Pasillas Villa <diana.pasillas@uacj.mx>
2024-12-05 09:30:22 -0700

    Manipulated Website Views and layout

--- SchoolManagement.MVC/Controllers/HomeController.cs
+++ SchoolManagement.MVC/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace SchoolManagement.MVC.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
